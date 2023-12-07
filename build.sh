fa1b9f0aa518624f63ecfefa2576051caf2ff7f8
Melany Velasquez <mel.montanov@gmail.com>
2023-12-07 19:25:57 -0300

    requirements

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-#exit on error
+# exit on error
 set -o errexit
-
-pip install -r requerements.txt
-
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
